80e145801288e182ab795f47acbb6d3b21dc18a6
Paul Legato <pjlegato@builtonlogic.com>
2014-07-28 16:43:38 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bureaucrat "0.2.2-SNAPSHOT"
+(defproject bureaucrat "0.2.2"
   :description "MQ-based API router"
   :url "https://github.com/pjlegato/bureaucrat/"
   :license {:name "Eclipse Public License"
